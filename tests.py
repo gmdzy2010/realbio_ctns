db004e707bc2028085115e977945496a3fcdebfe
yang_ge <gmdzy2010@users.noreply.github.com>
2018-06-29 00:14:46 +0800

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+
+class CtnsTest(TestCase):
+    def test_sms_notifications(self):
+        pass
+    
+    def test_tts_notifications(self):
+        pass
+    
+    def test_get_temperatures(self):
+        pass
+    
