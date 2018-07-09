7d3de4483a6e211faac9e3ba0a357b0e373e8a80
yang_ge <gmdzy2010@users.noreply.github.com>
2018-07-09 23:58:59 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,8 @@
-# for packaging this module
+from distutils.core import setup
+
+
+setup(
+    name='realbio_ctns',         # 应用名
+    version='2.0.2',        # 版本号
+    packages=['conf', 'core']    # 包括在安装包内的Python包
+)
